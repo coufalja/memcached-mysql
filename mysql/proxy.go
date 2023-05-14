282a0ceb9f542d41bf1b97a9c9f555c692b4c8a9
Jakub Coufal <jakub.coufal@jamf.com>
2023-05-14 08:50:28 +0200

    Refactoring

--- mysql/proxy.go
+++ mysql/proxy.go
@@ -12,62 +12,67 @@ import (
 	"github.com/mattrobenolt/go-memcached"
 )
 
-type MultiProxy struct {
-	p map[string]*SingleProxy
+const (
+	mappingPrefix  = "@@"
+	mappingSep     = "."
+	defaultMapping = "default"
+)
+
+type Proxy struct {
+	tables map[string]*tableProxy
 }
 
-func (c *MultiProxy) Get(key string) memcached.MemcachedResponse {
+func (c *Proxy) Get(key string) memcached.MemcachedResponse {
 	mapping, ckey, err := mappingKey(key)
 	if err != nil {
 		return &memcached.ClientErrorResponse{Reason: err.Error()}
 	}
-	if proxy, ok := c.p[mapping]; ok {
+	if proxy, ok := c.tables[mapping]; ok {
 		return proxy.Get(ckey)
 	}
 	return &memcached.ClientErrorResponse{Reason: fmt.Sprintf("no mapping present for a key: '%s'", key)}
 }
 
 func mappingKey(key string) (string, string, error) {
-	if strings.HasPrefix(key, "@@") {
-		sep := strings.Split(key, ".")
+	if strings.HasPrefix(key, mappingPrefix) {
+		sep := strings.Split(key, mappingSep)
 		if len(sep) < 2 {
 			return "", "", errors.New("bad key format")
 		}
-		return strings.TrimLeft(sep[0], "@"), sep[1], nil
+		return strings.TrimLeft(sep[0], mappingPrefix), sep[1], nil
 	}
-	return "default", key, nil
+	return defaultMapping, key, nil
 }
 
-func New(db *sql.DB, mapping []config.Mapping) *MultiProxy {
-	proxy := &MultiProxy{
-		p: make(map[string]*SingleProxy),
+func New(db *sql.DB, mapping []config.Mapping) *Proxy {
+	proxy := &Proxy{
+		tables: make(map[string]*tableProxy),
 	}
 	for _, m := range mapping {
 		stmt, err := db.Prepare(fmt.Sprintf("SELECT %s FROM %s WHERE %s=?", m.ValueColumn, m.Table, m.KeyColumn))
 		if err != nil {
 			panic(err)
 		}
-		proxy.p[m.Name] = &SingleProxy{
+		proxy.tables[m.Name] = &tableProxy{
 			query: stmt,
 		}
 	}
 	return proxy
 }
 
-type SingleProxy struct {
+type tableProxy struct {
 	query *sql.Stmt
 }
 
-func (c *SingleProxy) Get(key string) memcached.MemcachedResponse {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+func (c *tableProxy) Get(key string) memcached.MemcachedResponse {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	row := c.query.QueryRowContext(ctx, key)
 	if row.Err() != nil {
 		return &memcached.ClientErrorResponse{Reason: row.Err().Error()}
 	}
 	res := ""
-	err := row.Scan(&res)
-	if err != nil {
+	if err := row.Scan(&res); err != nil {
 		return &memcached.ClientErrorResponse{Reason: err.Error()}
 	}
 	return &memcached.ItemResponse{Item: &memcached.Item{Key: key, Value: []byte(res)}}
